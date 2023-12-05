f9a1da9a849fa1d1bb8583984f7cf7ce85631d44
KazumaEnomoto <kk.yzgb1310@gmail.com>
2023-12-05 18:15:38 +0900

    Adminページの雛形作成

---
+++ front/src/AppAdmin.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AppAdmin: React.FC = () => {
+    return (
+        <div className="AppAdmin">
+            AppAdminです。
+        </div>
+    )
+}
+
+export default AppAdmin;
